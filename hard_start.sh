f58c127141ed71b47490a37ecd1fed7cf9fe6282
tnick1502 <tnick1502@mail.ru>
2026-03-06 16:01:36 +0300

    fix

--- hard_start.sh
+++ hard_start.sh
@@ -3,4 +3,4 @@ docker rm $(docker ps -a -q) -f
 docker rmi $(docker images -a -q) -f
 docker system prune -a -f
 git pull
-docker-compose up  --force-recreate --build -d
+docker-compose -f docker-compose-test.yml up --build -d
